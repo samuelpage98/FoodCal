9a670d65f5e3ba76be99a81d1e1233ad50fc1f08
Sagar Shah <s.d.shah@accenture.com>
2022-01-24 11:26:40 +0000

    conflicts resolved

--- food-cal/src/App.js
+++ food-cal/src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import Dashboard from './pages/Dashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Dashboard from './pages/Dashboard'
+import Calendar from './pages/Calendar'
+import MealLibrary from './pages/MealLibrary'
+import ShoppingList from './pages/ShoppingList'
 
 function App() {
   return (
-    <Dashboard/>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<Dashboard />} />
+        <Route path="calendar" element={<Calendar />} />
+        <Route path="meal-library" element={<MealLibrary />} />
+        <Route path="shopping-list" element={<ShoppingList />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- food-cal/src/pages/Dashboard.js
+++ food-cal/src/pages/Dashboard.js
@@ -3,15 +3,15 @@ import Button from '@mui/material/Button';
 // import Stack from '@mui/material/Stack';
 
 function Dashboard() {
-    return (
-        <>
-            <Button variant="contained">Calendar</Button>
-            <Button variant="contained">Meal Library</Button>
-            <Button variant="contained">Shopping List</Button>
-        </>
+  return (
+    <>
+      <Button variant="contained">Calendar</Button>
+      <Button variant="contained">Meal Library</Button>
+      <Button variant="contained">Shopping List</Button>
+    </>
 
-    );
-  }
-  
-  export default Dashboard;
-  
+  );
+}
+
+export default Dashboard;
+
