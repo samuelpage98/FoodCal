1d40d58d44a32fd49768bc9ddf277cc9d08a17a2
Sagar Shah <s.d.shah@accenture.com>
2022-01-24 15:46:15 +0000

    created basic dashboard widget

---
+++ food-cal/src/components/DashboardGreeting.js
@@ -0,0 +1,9 @@
+function DashboardGreeting({ name }) {
+    return (
+        <div className="dashboardWidget">
+            <h1>Welcome {name}</h1>
+        </div>
+    )
+}
+
+export default DashboardGreeting
--- food-cal/src/components/DashboardPanel.js
+++ food-cal/src/components/DashboardPanel.js
@@ -1,15 +1,13 @@
 import { Link } from "react-router-dom"
 import { Button } from "@mui/material"
 import DashboardCard from '../components/DashboardCard'
+import DashboardGreeting from "./DashboardGreeting"
 
 function DashboardPanel() {
     return (
         <>
             <h1>Dashboard</h1>
-            {/* <Link to="/calendar"><Button variant="contained">Calendar</Button></Link>
-            <Link to="/meal-library"><Button variant="contained">Meal Library</Button></Link>
-            <Link to="/shopping-list"><Button variant="contained">Shopping List</Button></Link> */}
-            <DashboardCard width={1530} height={200} />
+            <DashboardGreeting name="Steven Hawking" />
             <div style={{ display: "flex" }}>
                 <DashboardCard width={500} height={500} />
                 <DashboardCard width={500} height={500} />
