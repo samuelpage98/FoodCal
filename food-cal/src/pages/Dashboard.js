72528451c6216dd8dbdbfb64c27dbf9d55dc573c
samuelpage98 <samuelpage_@hotmail.com>
2022-01-24 12:19:03 +0000

    added checklist functionality

--- food-cal/src/pages/Dashboard.js
+++ food-cal/src/pages/Dashboard.js
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import Button from '@mui/material/Button';
 import { Link } from "react-router-dom";
+import CheckList from '../components/shoppingCheckList';
 // import Stack from '@mui/material/Stack';
 
 function Dashboard() {
@@ -9,6 +10,7 @@ function Dashboard() {
       <Link to="/calendar"><Button variant="contained">Calendar</Button></Link>
       <Link to="/meal-library"><Button variant="contained">Meal Library</Button></Link>
       <Link to="/shopping-list"><Button variant="contained">Shopping List</Button></Link>
+      <CheckList />
     </>
 
   );
