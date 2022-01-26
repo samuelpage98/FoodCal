c076cd49d4a631124afe97cf13704ca8ba202720
samuelpage98 <samuelpage_@hotmail.com>
2022-01-26 10:53:25 +0000

    added meal card component

--- food-cal/src/components/MealLibraryPanel.js
+++ food-cal/src/components/MealLibraryPanel.js
@@ -1,6 +1,11 @@
+import MealCard from "./mealCard"
+
 function MealLibraryPanel() {
     return (
-        <h1>Meal Library</h1>
+        <>
+            <h1>Meal Library</h1>
+            <MealCard mealName="Jacket Potato" mealDescription="We have here, a wild potato in a north face jacket" imageURL="https://images.pexels.com/photos/5377338/pexels-photo-5377338.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260" />
+        </>
     )
 }
 
