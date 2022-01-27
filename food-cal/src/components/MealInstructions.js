1f3d7651cf6f9fd012d96b8056178ab8cad39629
Sagar Shah <s.d.shah@accenture.com>
2022-01-27 15:19:18 +0000

    clean up code

--- food-cal/src/components/MealInstructions.js
+++ food-cal/src/components/MealInstructions.js
@@ -1,30 +1,25 @@
+/* eslint-disable no-lone-blocks */
 import * as React from 'react';
 import Box from '@mui/material/Box';
-import Button from '@mui/material/Button';
+
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
-import { TextField } from '@mui/material';
-import { Switch } from '@mui/material';
-import { FormControlLabel } from '@mui/material';
-import { Stack } from '@mui/material';
-import { styled } from '@mui/material/styles';
 
-const apiURL = ' https://tpua2ywmzj.execute-api.us-east-1.amazonaws.com/';
+
+
 
 const style = {
     position: 'absolute',
     top: '50%',
     left: '50%',
     transform: 'translate(-50%, -50%)',
-    width: 400,
+    width: 630,
     bgcolor: 'background.paper',
     border: '2px solid #000',
     boxShadow: 24,
     p: 4,
 };
-const Input = styled('input')({
-    display: 'none',
-});
+
 
 export default function MealInstructions(props) {
 
@@ -47,18 +42,19 @@ export default function MealInstructions(props) {
                                 <Box
                                     component="form"
                                     sx={{
-                                        '& > :not(style)': { m: 1, width: '25ch' },
+                                        '& > :not(style)': { m: 1, width: '60ch' },
                                     }}
                                     noValidate
                                     autoComplete="off"
                                 >
+                                    <h3>Ingredients</h3>
                                     <ul>
                                         {props.mealData[props.mealCardIndex].recipe.M.ingredients.L.map(el => {
                                             return <li>{el.S}: {props.mealData[props.mealCardIndex].recipe.M.measurements.L[props.mealData[props.mealCardIndex].recipe.M.ingredients.L.indexOf(el)].S}</li>
                                         })}
                                     </ul>
-
-
+                                    <h3>Method</h3>
+                                    <p>{props.mealData[props.mealCardIndex].method.S}</p>
                                 </Box>
                             </Typography>
                         </Box>
