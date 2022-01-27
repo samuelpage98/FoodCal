56c12dbdeff2cb6cef1f80c84167bd2b66ede193
samuelpage98 <samuelpage_@hotmail.com>
2022-01-27 10:54:46 +0000

    instructions works for mymeals as well

--- food-cal/src/components/MyMealsCardPanel.js
+++ food-cal/src/components/MyMealsCardPanel.js
@@ -1,10 +1,12 @@
 import { useEffect, useState } from "react";
 import MealCard from "./mealCard";
+import MealInstructions from "./MealInstructions";
 
 const apiURL = 'https://tpua2ywmzj.execute-api.us-east-1.amazonaws.com/';
 
 function MealsCardPanel() {
     const [mealData, setMealData] = useState([]);
+    const [mealCardIndex, setMealCardIndex] = useState();
     useEffect(async () => {
         let response = await fetch(apiURL + 'meals', {
             method: 'GET',
@@ -19,14 +21,22 @@ function MealsCardPanel() {
         console.log(await data)
     }, [])
 
+    const [open, setOpen] = useState(false);
+    const handleClose = () => setOpen(false);
+
+    const instructionButtonHandler = (index) => {
+        setOpen(true);
+        setMealCardIndex(index)
+    }
+
     return (
         <>
             {mealData.map(el => {
                 if (el.inMyMeal.BOOL) {
-                    return <MealCard imageURL={el.imageURL.S} mealName={el.mealName.S} mealDescription="This is the meal description, to be added to db later or maybe not" />
+                    return <MealCard index={mealData.indexOf(el)} instructionButtonHandler={instructionButtonHandler} setMealCardIndex={setMealCardIndex} imageURL={el.imageURL.S} mealName={el.mealName.S} mealDescription="This is the meal description, to be added to db later or maybe not" />
                 }
             })}
-
+            <MealInstructions mealCardIndex={mealCardIndex} open={open} handleClose={handleClose} mealData={mealData} />
         </>
     )
 }
